8bb13409e61bf8ddefb673d3c31e7bdea774a0aa
mahdi-21 <83784541+mahdi-21@users.noreply.github.com>
2023-01-31 11:13:12 +0300

    Initial commit

---
+++ my_js/script/main.js
@@ -0,0 +1,16 @@
+/* function to get to the element when page is on load when we put script tag in head */
+
+// window.onload=function(){
+// document.querySelector("h1").style.color="blue"}
+
+
+
+
+
+/* this case when we put the script tag at the end of body and there will be no errors*/
+//document.querySelector("h1").style.color ="blue" ;
+
+
+document.querySelector("h2").style.color="red";
+
+console.log("Hi from js file");
